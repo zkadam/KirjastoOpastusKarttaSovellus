be243e093f1c5a1e113ec617eb9633bcc8383a14
zkadam <adam.zakar@student.careeria.fi>
2020-09-18 15:52:14 +0300

    kuva lisäys koodattu

---
+++ Controllers/KuvatController.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using KirjastoAppScrum.Models;
+
+namespace KirjastoAppScrum.Controllers
+{
+    public class KuvatController : Controller
+    {
+        private KirjastoProjektiEntities1 db = new KirjastoProjektiEntities1();
+
+        // GET: Kuvat
+        public ActionResult Index()
+        {
+            return View(db.Kuvat.ToList());
+        }
+
+        // GET: Kuvat/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Kuvat kuvat = db.Kuvat.Find(id);
+            if (kuvat == null)
+            {
+                return HttpNotFound();
+            }
+            return View(kuvat);
+        }
+
+        // GET: Kuvat/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Kuvat/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "KuvaId,AltText,ImagePath,Image,kuva")] Kuvat kuvat)
+        {
+            if (ModelState.IsValid)
+            {
+                int count = Request.Files.Count;
+                var file = Request.Files[0];
+                string filename = file.FileName;
+                byte[] buffer = new byte[file.InputStream.Length];
+                file.InputStream.Read(buffer, 0, (int)file.InputStream.Length);
+
+                db.Entry(kuvat).State = EntityState.Added;
+                kuvat.Image = buffer;
+                kuvat.ImagePath = filename;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(kuvat);
+        }
+
+        // GET: Kuvat/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Kuvat kuvat = db.Kuvat.Find(id);
+            if (kuvat == null)
+            {
+                return HttpNotFound();
+            }
+            return View(kuvat);
+        }
+
+        // POST: Kuvat/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "KuvaId,AltText,ImagePath,Image")] Kuvat kuvat)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(kuvat).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(kuvat);
+        }
+
+        // GET: Kuvat/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Kuvat kuvat = db.Kuvat.Find(id);
+            if (kuvat == null)
+            {
+                return HttpNotFound();
+            }
+            return View(kuvat);
+        }
+
+        // POST: Kuvat/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Kuvat kuvat = db.Kuvat.Find(id);
+            db.Kuvat.Remove(kuvat);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
